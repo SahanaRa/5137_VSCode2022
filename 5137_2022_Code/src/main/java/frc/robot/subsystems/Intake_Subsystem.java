f96d2f9ea508d780b3bb8608dd53b97659c1a142
SahanaRa <sahananr13@gmail.com>
2022-01-20 17:07:17 -0800

    Deleted template command.

--- 5137_2022_Code/src/main/java/frc/robot/subsystems/Intake_Subsystem.java
+++ 5137_2022_Code/src/main/java/frc/robot/subsystems/Intake_Subsystem.java
@@ -7,8 +7,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
@@ -18,7 +16,7 @@ import frc.robot.RobotContainer;
 //motor variables 
 
 public class Intake_Subsystem extends SubsystemBase { 
-
+    boolean tesChngeBooolean;
     boolean intakeDown = false;
     boolean toggleIntakeDirection = false;
 
